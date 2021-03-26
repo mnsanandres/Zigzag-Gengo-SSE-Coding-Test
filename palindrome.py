473d4cc415f186a5fd0b8492b9e149f574c8316a
Mervin San Andres <vinsanandres@gmail.com>
2021-03-26 17:23:29 +0800

    Added initial solutions

---
+++ palindrome.py
@@ -0,0 +1,40 @@
+def is_palindrome(string):
+    assert (isinstance(string, str) is True)
+    return string == string[::-1]
+
+
+def longest_palindrome(string):
+    combo = _subsets(string)
+    longest = ''
+    for word in combo:
+        if len(word) > len(longest) and is_palindrome(word):
+            longest = word
+    return longest
+
+
+def minimum_palindrome_cut(string):
+    current = string
+    combo = _subsets(string)
+    combo = sorted(combo, key=lambda x: len(x))[::-1]
+    parts = 0
+    for sub in combo:
+        if is_palindrome(sub) and sub in current:
+            # print(sub)
+            parts += 1
+            current = current.replace(sub, '')
+        if not current:
+            break
+    return parts - 1
+
+
+def _subsets(string):
+    return [string[i: j] for i in range(len(string)) for j in range(i + 1, len(string) + 1)]
+
+
+if __name__ == '__main__':
+    assert (is_palindrome('a') is True)
+    assert (is_palindrome('ab') is False)
+    assert (is_palindrome('abba') is True)
+    assert (is_palindrome('madamimeve') is False)
+    assert (longest_palindrome('abaxyzzyxf') == 'xyzzyx')
+    assert (minimum_palindrome_cut('noonabbad') == 2)
---
+++ palindrome_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+import palindrome
+
+
+class MyTestCase(unittest.TestCase):
+    def test_invalid_input(self):
+        with self.assertRaises(AssertionError):
+            palindrome.is_palindrome(1)
+
+    def test_palindrome(self):
+        self.assertEqual(palindrome.is_palindrome(''), True)
+        self.assertEqual(palindrome.is_palindrome('a'), True)
+        self.assertEqual(palindrome.is_palindrome('ab'), False)
+        self.assertEqual(palindrome.is_palindrome('abba'), True)
+        self.assertEqual(palindrome.is_palindrome('racecar'), True)
+        self.assertEqual(palindrome.is_palindrome('madamimadam'), True)
+        self.assertEqual(palindrome.is_palindrome('madamimeve'), False)
+
+    def test_longest_palindrome(self):
+        self.assertEqual(palindrome.longest_palindrome('abaxyzzyxf'), 'xyzzyx')
+        self.assertEqual(palindrome.longest_palindrome('madam referred a civic racecar model'), ' racecar ')
+
+    def test_minimum_palindrome_cut(self):
+        self.assertEqual(palindrome.minimum_palindrome_cut('noonabbad'), 2)
+        self.assertEqual(palindrome.minimum_palindrome_cut('racecar'), 0)
+        self.assertEqual(palindrome.minimum_palindrome_cut('11racecarevents'), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
